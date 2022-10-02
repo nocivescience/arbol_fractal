1d532909e9ea394eb1d4b671dac187e865a34baf
noceive <nocivescience@gmail.com>
2022-10-02 02:16:39 -0300

    functiona con una rama

--- arbol.py
+++ arbol.py
@@ -1,22 +1,30 @@
-from distutils.command.build_scripts import first_line_re
-from turtle import down
 from manim import *
 class TreeScene(Scene):
     CONFIG={
-        'iteraciones':3
+        'iteraciones':5
     }
     def construct(self):
-        ghost_line=Line(3*DOWN,4*DOWN).fade(1)
-        self.first_line(ghost_line,)
+        ghost_line=Line(3*DOWN,4*DOWN).fade(1).center()
+        self.first_line(ghost_line)
+        for _ in range(self.CONFIG['iteraciones']):
+            self.first_line(self.line_left,-PI/2+self.line_left.get_angle(),self.line_left.get_length()/2)
     def first_line(self,path,angle=0,length=3):
         line=Line(UP,DOWN).rotate(angle).set_length(length)
-        line.move_to(path.points[0],line.points[-1])
-        line_left,line_right=line.copy(),line.copy()
+        line.move_to(path.points[-1],line.points[0])
+        self.line_left,self.line_right=line.copy(),line.copy()
         self.play(Create(line))
-        self.wait()
         self.play(*[
             Rotate(
                 mob,angle,about_point=line.points[0]
-            ) for mob,angle in zip([line_left,line_right],[-140*DEGREES, 140*DEGREES])
+            ) for mob,angle in zip([self.line_left,self.line_right],[-140*DEGREES, 140*DEGREES])
         ])
-        self.wait()
+    def first_line_2(self,path,angle=0,length=3):
+        line=Line(UP,DOWN).rotate(angle).set_length(length)
+        line.move_to(path.points[-1],line.points[0])
+        self.line_left,self.line_right=line.copy(),line.copy()
+        self.play(Create(line))
+        self.play(*[
+            Rotate(
+                mob,angle,about_point=line.points[0]
+            ) for mob,angle in zip([self.line_left,self.line_right],[-140*DEGREES, 140*DEGREES])
+        ])
